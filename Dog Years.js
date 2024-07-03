32d921fd69c6ac80c9bbe21a943552c670158b83
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-07-03 18:49:22 +0600

    convert human year to dog year

---
+++ Dog Years.js
@@ -0,0 +1,14 @@
+let myAge = 21;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears /= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Azizur Rahaman".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
