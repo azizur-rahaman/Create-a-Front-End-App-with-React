6d4ecb3a9145da11c05d1b1c7420f31402cbf036
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-07-03 18:49:49 +0600

    convert kelvin to celcious and farenhait

---
+++ Kelvin Weather.js
@@ -0,0 +1,11 @@
+// Kelvin Weather
+let kelvin = 293;
+
+
+// Celsius
+let celcius = kelvin - 273;
+
+// Fahrenheit
+let farenheit = Math.floor(celcius * (9/5) + 32);
+
+console.log(`The temperature is ${farenheit} degrees Fahrenheit.`)
